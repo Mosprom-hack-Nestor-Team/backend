4f4bab6d931033ed945d401e71969d1f5edfeac9
Valeriy Kamenskikh <vkamenskih@play.team>
2025-10-18 15:50:26 +0300

    feat: Update docker file and change env

--- run.py
+++ run.py
@@ -3,11 +3,12 @@ Simple script to run the FastAPI application
 Usage: python3 run.py
 """
 import uvicorn
+from app.core.config import settings
 
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
-        host="0.0.0.0",
-        port=7878,
-        reload=True,
+        host=settings.HOST,
+        port=settings.PORT,
+        reload=settings.RELOAD,
     )
